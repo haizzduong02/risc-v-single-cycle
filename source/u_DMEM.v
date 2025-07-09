6cd475100c45cb702579f66e1bb00b17bbd5b3e8
haizzduong <haiduongnguyen17@gmail.com>
2025-07-09 15:18:05 +0700

    dmem fix

--- source/u_DMEM.v
+++ source/u_DMEM.v
@@ -1,10 +1,10 @@
 module DMEM #(
     parameter           MEM_DEPTH = 256
 ) (
-    output  reg [31:0]  data_r,
+    output  reg [31:0]  dataR,
     
     input       [31:0]  addr,
-    input       [31:0]  data_w,
+    input       [31:0]  dataW,
     input       [ 2:0]  load_sel,
     input       [ 1:0]  store_sel,
     input               clk,
@@ -20,12 +20,12 @@ module DMEM #(
     always @(*) begin
         data = (addr[31:2] < MEM_DEPTH) ? memory[addr[31:2]] : {32{1'b0}};
         case (load_sel)
-            `LOAD_SEL_B:    data_r = {{24{data[7]}},    data[7:0]};
-            `LOAD_SEL_BU:   data_r = {{24{1'b0}},       data[7:0]};
-            `LOAD_SEL_H:    data_r = {{16{data[15]}},   data[15:0]};
-            `LOAD_SEL_HU:   data_r = {{16{1'b0}},       data[15:0]};
-            `LOAD_SEL_W:    data_r = data;
-            default:        data_r = data;
+            `LOAD_SEL_B:    dataR = {{24{data[7]}},    data[7:0]};
+            `LOAD_SEL_BU:   dataR = {{24{1'b0}},       data[7:0]};
+            `LOAD_SEL_H:    dataR = {{16{data[15]}},   data[15:0]};
+            `LOAD_SEL_HU:   dataR = {{16{1'b0}},       data[15:0]};
+            `LOAD_SEL_W:    dataR = data;
+            default:        dataR = data;
         endcase
     end
 
@@ -39,21 +39,21 @@ module DMEM #(
                 case (store_sel)
                     `STORE_SEL_B: begin
                         case (addr[1:0])
-                            2'b00: memory[addr[31:2]][7:0]   <= data_w[7:0];
-                            2'b01: memory[addr[31:2]][15:8]  <= data_w[7:0];
-                            2'b10: memory[addr[31:2]][23:16] <= data_w[7:0];
-                            2'b11: memory[addr[31:2]][31:24] <= data_w[7:0];
+                            2'b00: memory[addr[31:2]][7:0]   <= dataW[7:0];
+                            2'b01: memory[addr[31:2]][15:8]  <= dataW[7:0];
+                            2'b10: memory[addr[31:2]][23:16] <= dataW[7:0];
+                            2'b11: memory[addr[31:2]][31:24] <= dataW[7:0];
                         endcase
                     end
                     `STORE_SEL_H: begin
                         case (addr[1:0])
-                            2'b00: memory[addr[31:2]][15:0]  <= data_w[15:0];
-                            2'b10: memory[addr[31:2]][31:16] <= data_w[15:0];
+                            2'b00: memory[addr[31:2]][15:0]  <= dataW[15:0];
+                            2'b10: memory[addr[31:2]][31:16] <= dataW[15:0];
                             default: ; 
                         endcase
                     end
                     `STORE_SEL_W: begin
-                        memory[addr[31:2]] <= data_w;
+                        memory[addr[31:2]] <= dataW;
                     end
                     default: ;
                 endcase
